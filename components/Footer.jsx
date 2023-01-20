9d882042b7da3f7090b3b5c9ac3196af28b71120
Kinga <kinga@radcode.co>
2023-01-20 13:16:52 +0100

    added product details

---
+++ components/Footer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
+
+const Footer = () => {
+  return <div className="footer-container"><p>2022 Kinga All Rights Reserved</p> <p className="icons"><AiFillInstagram /><AiOutlineTwitter /></p></div>;
+};
+
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,34 @@
 import React from "react";
+import { client } from "../lib/client";
+import { Product, FooterBanner, HeroBanner } from "../components";
 
-const Home = () => {
-  return (
-    <>
-      HeroBanner
-      <div className="products-heading">
-        <h2>Bestsellers</h2>
-        <p>Unleash your dog's creativity - joyful freedom </p>
-      </div>
-      <div className="products-container">
-        {["Product 1", "Product 2"].map((product) => product)}
-      </div>
-      Footer
-    </>
-  );
+const Home = ({ products, bannerData }) => (
+  <div>
+    <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+
+    <div className="products-heading">
+      <h2>Bestsellers</h2>
+      <p>Unleash your dogs creativity - joyful freedom </p>
+    </div>
+    <div className="products-container">
+      {products?.map((product) => (
+        <Product key={product._id} product={product} />
+      ))}
+    </div>
+    <FooterBanner FooterBanner={bannerData && bannerData[0]}/>
+  </div>
+);
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
 };
 
 export default Home;
